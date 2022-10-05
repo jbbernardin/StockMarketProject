1102452da569c987310f71fcd40696ba62bef1e1
jbbernardin <62387765+jbbernardin@users.noreply.github.com>
2022-10-04 20:14:45 -0400

    Found Max a from Fixed Probability

--- FavorableOdds.m
+++ FavorableOdds.m
@@ -3,47 +3,36 @@ clc
 
 screen = get( groot, 'Screensize' );
 n = 1000;
-a = (0:.01:1);
-N = size(a,2);
+p = .75;
+X0 = 100;
+a = 0:.01:1;
+len = length(a);
+N = 10000;
+avgrap = zeros(1,len);
 
-r = @(x) 1/n*log(100*(1+x)^n);
-dr = @(x) 1/(1+x);
-
-ra = zeros(1,N);
-dra = zeros(1,N);
-
-for k = 1:N
-    ra(k) = r(a(k));
-    dra(k) = dr(a(k));
-end
-
-localmaxmin = (dra == 0);
-first = ra(1);
-last = ra(N);
-
-max = intmin;
-maxa = -1;
-for k = 1:N
-    if localmaxmin(k) == 1
-        if max<ra(k)
-            max = ra(k);
-            maxa = a(k);
+for k = 1:len
+    rap = zeros(1,N);
+    for m = 1:N
+        Xn = X0;
+        for i = 1:n
+            if rand < p
+                Xn = Xn*(1+a(k));
+            else
+                Xn = Xn*(1-a(k));
+            end
         end
+        rap(m) = 1/n*log(Xn);
     end
+    avgrap(k) = mean(rap);
 end
-if max<first
-    max = first;
-    maxa = a(1);
-end
-if max<last
-    max = last;
-    maxa = a(N);
-end
+
+[rmax,maxIndex] = max(avgrap);
+amax = a(maxIndex);
 
 figure('Position',[screen(1),screen(2)+ceil((5/72)*screen(4)),screen(3),screen(4)-ceil((25/144)*screen(4))])
-plot(a,ra)
-txt = ['\leftarrow Optimal a = ', num2str(maxa)];
-title('Profit if Betting in Favor and Winning Every Time')
-xlabel('Percent in Favor')
-ylabel('Best Case Results')
-text(maxa,ra(maxa),txt)
+plot(a,avgrap)
+title(['Average r(a,p) with Fixed p = ', num2str(p), ' and Varied a'])
+ylabel('r(a,p)')
+xlabel('a')
+txt = ['\leftarrow Optimal a = ', num2str(amax)];
+text(amax,rmax,txt)
